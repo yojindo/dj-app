0f9ce65f0c0bd9a4bec4652cb601d67f440bdb0c
Jake Kwak <jakejaekwak@gmail.com>
2023-04-15 22:49:00 -0400

    fix chatgpt endpoint and cleanup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,25 +5,49 @@ import { useState } from 'react';
 
 function App() {
   const [song, setSong] = useState('');
+  const [songQuery, setSongQuery] = useState('');
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
-    console.log('form submitted', song);
+    const generatedSong = await generateSong();
+    setSongQuery(generatedSong);
+  };
+
+  const generateSong = async () => {
+    const response = await fetch('http://localhost:3009/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ song }),
+    });
+
+    const data = await response.json();
+    return data.response.trim();
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      onSubmit(e);
+    }
   };
 
   return (
     <main className={styles.main}>
       <img src={djLogo} alt="vinyl" className={styles.icon} />
-      <h3>Generate songs with AI</h3>
+      <h3>Generate song recommendations with AI</h3>
 
       <form onSubmit={onSubmit}>
         <input
           type="text"
           name="query-description"
-          placeholder="song title and artist"
+          placeholder="song and artist"
           onChange={(e) => setSong(e.target.value)}
+          onKeyPress={handleKeyPress}
         />
-        <input type="submit" value="song" />
+        <input type="submit" value="Generate" />
+        <pre className={styles.result}>{songQuery}</pre>
       </form>
     </main>
   );
